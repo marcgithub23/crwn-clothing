e9a92b0464d331abe7cbd393281fa7efaad00af5
marcgithub23 <marc.dagohoy2@gmail.com>
2025-02-16 16:19:47 +0000

    Implement checkout page, checkout items, checkout cart functionalities

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,40 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
+import './checkout-item.styles.scss';
+
+const CheckoutItem = ({cartItem}) => {
+  const {name, imageUrl, price, quantity} = cartItem;
+
+  const { clearItemFromCart, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
+
+  const clearItemHandler = () => clearItemFromCart(cartItem);
+  const addItemHandler = () => addItemToCart(cartItem);
+  const removeItemHandler = () => removeItemFromCart(cartItem);
+
+  return (
+    <div className='checkout-item-container'>
+      <div className='image-container'>
+        <img src={imageUrl} alt={`${name}`} />
+      </div>
+      <span className='name'> {name} </span>
+      <div className='quantity'>
+        <span className='arrow' onClick={removeItemHandler}>
+          &#10094;
+        </span>
+        <span className='value'>{quantity}</span>
+        <span className='arrow' onClick={addItemHandler}>
+          &#10095;
+        </span>
+      </div>
+      <span className='price'> {price} </span>
+      <span className='remove-button' onClick={clearItemHandler}>
+        &#10005;
+      </span>
+    </div>
+  );
+}
+
+export default CheckoutItem;
