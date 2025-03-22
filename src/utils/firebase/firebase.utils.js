6d2cceecf36f4c2a2c800f4debdb8ecb4bf4886d
marcgithub23 <marc.dagohoy2@gmail.com>
2025-03-22 21:28:06 +0000

    Fix errors for deployment

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -29,7 +29,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({
