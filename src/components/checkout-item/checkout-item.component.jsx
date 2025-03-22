6894b57ac605c37d29b010e21ed70bb2286b2111
marcgithub23 <marc.dagohoy2@gmail.com>
2025-03-22 21:01:08 +0000

    Deploy on Netlify

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,10 +2,18 @@ import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart.context';
 
-import './checkout-item.styles.scss';
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  BaseSpan,
+  Quantity,
+  Arrow,
+  Value,
+  RemoveButton,
+} from './checkout-item.styles';
 
-const CheckoutItem = ({cartItem}) => {
-  const {name, imageUrl, price, quantity} = cartItem;
+const CheckoutItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
 
   const { clearItemFromCart, addItemToCart, removeItemFromCart } =
     useContext(CartContext);
@@ -15,26 +23,20 @@ const CheckoutItem = ({cartItem}) => {
   const removeItemHandler = () => removeItemFromCart(cartItem);
 
   return (
-    <div className='checkout-item-container'>
-      <div className='image-container'>
+    <CheckoutItemContainer>
+      <ImageContainer>
         <img src={imageUrl} alt={`${name}`} />
-      </div>
-      <span className='name'> {name} </span>
-      <div className='quantity'>
-        <span className='arrow' onClick={removeItemHandler}>
-          &#10094;
-        </span>
-        <span className='value'>{quantity}</span>
-        <span className='arrow' onClick={addItemHandler}>
-          &#10095;
-        </span>
-      </div>
-      <span className='price'> {price} </span>
-      <span className='remove-button' onClick={clearItemHandler}>
-        &#10005;
-      </span>
-    </div>
+      </ImageContainer>
+      <BaseSpan> {name} </BaseSpan>
+      <Quantity>
+        <Arrow onClick={removeItemHandler}>&#10094;</Arrow>
+        <Value>{quantity}</Value>
+        <Arrow onClick={addItemHandler}>&#10095;</Arrow>
+      </Quantity>
+      <BaseSpan> {price}</BaseSpan>
+      <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
+    </CheckoutItemContainer>
   );
-}
+};
 
 export default CheckoutItem;
